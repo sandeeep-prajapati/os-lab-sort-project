791555035d916b1140ddea2a4b660fd0a6a9c2a8
sandeep prajapati <89738709+sandeeep-prajapati@users.noreply.github.com>
2023-04-27 16:12:14 +0530

    part time in os lab in fun

---
+++ index.js
@@ -0,0 +1,36 @@
+/* Please ❤ this and follow me if you like it! */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Working with credits
+
+var cpCreditsUrl = "https://codepen.io/PickJBennett/details/BdbrMW";
+var cpCreditsTitle = "🌈 Sexy Animated Rainbow Waves Header";
+var cpCreditsTwitter = document.getElementById("js-tweet-this");
+cpCreditsTwitter.href = "https://twitter.com/intent/tweet?text=" + encodeURI(cpCreditsTitle) + "&url=" + encodeURI(cpCreditsUrl) + "&via=PickJBennett&related=PickJBennett,CiaoFileManager";
+cpCreditsTwitter.innerHTML += "Tweet This Pen";
